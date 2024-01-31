d70277da4ef9cde261e2c160b36d763cbf562584
udit <uditkumawat7@gmail.com>
2024-01-31 11:22:26 +0530

    kite

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -61,7 +61,7 @@ function Header() {
 							<div className="sm:flex sm:gap-4">
 								<a
 									className="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700 "
-									href="/files"
+									href="/upload"
 								>
 									Get Started
 								</a>
